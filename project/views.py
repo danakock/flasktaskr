fcbfe602f9d338ae1791682c9794434dea15964e
Dana Kock <dkock@eventbrite.com>
2015-06-10 07:06:51 -0700

    Add Register Path

--- project/views.py
+++ project/views.py
@@ -1,10 +1,9 @@
-# project/views.py 
-
-from forms import AddTaskForm
+# imports
+from forms import AddTaskForm, RegisterForm, LoginForm
 
 from functools import wraps
 from flask import Flask, flash, redirect, render_template, \
-request, session, url_for, g
+request, session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # config
@@ -12,7 +11,7 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 # helper functions
 
@@ -103,6 +102,23 @@ def delete_entry(task_id):
 	db.session.commit()
 	flash("The task has been deleted!")
 	return redirect(url_for('tasks'))
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+	error = None
+	form = RegisterForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			new_user = User(
+				form.name.data,
+				form.email.data,
+				form.password.data,
+			)
+			db.session.add(new_user)
+			db.session.commit()
+			flash('Thanks for registering. Please login.')
+			return redirect(url_for('login'))
+	return render_template('register.html', form=form, error=error)
 
 
 
