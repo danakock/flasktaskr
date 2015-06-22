d046e901c6f615615d26a9bea4d4c598c8915c73
Dana Kock <dkock@eventbrite.com>
2015-06-22 07:49:29 -0700

    Add Admin Functionality

--- project/views.py
+++ project/views.py
@@ -40,19 +40,22 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 
+
 def flash_errors(form):
-	for field, errors in form.errors.items():
-		for error in errors:
-			flash(u"Error in the %s field - %s" % ( 
-				getattr(form, field).label.text, error), 'error')
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text, error), 'error')
+
 
 def open_tasks():
-	return db.session.query(Task).filter_by(
-		status='1').order_by(Task.due_date.asc())
+    return db.session.query(Task).filter_by(
+        status='1').order_by(Task.due_date.asc())
+
 
 def closed_tasks():
-	return db.session.query(Task).filter_by(
-		status='0').order_by(Task.due_date.asc())
+    return db.session.query(Task).filter_by(
+        status='0').order_by(Task.due_date.asc())
 
 
 ########################
@@ -64,6 +67,7 @@ def closed_tasks():
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
+    session.pop('role', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -78,6 +82,7 @@ def login():
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
                 session['user_id'] = user.id
+                session['role'] = user.role
                 flash('Welcome!')
                 return redirect(url_for('tasks'))
             else:
@@ -97,15 +102,14 @@ def register():
                 form.password.data,
             )
             try:
-            	db.session.add(new_user)
-            	db.session.commit()
-            	flash('Thanks for registering. Please login.')
-            	return redirect(url_for('login'))
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
             except IntegrityError:
-            	error = 'That username and/or email already exist!'
-    		return render_template('register.html', form=form, error=error)
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
-
 
 
 @app.route('/tasks/')
@@ -122,6 +126,7 @@ def tasks():
 @app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
+    error = None
     form = AddTaskForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -138,31 +143,41 @@ def new_task():
             flash('New entry was successfully posted. Thanks.')
             return redirect(url_for('tasks'))
     return render_template(
-    	'tasks.html', 
-    	form=form, 
-    	error=error,
-    	open_tasks=open_tasks(),
-    	closed_tasks=closed_tasks()
-    	)
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
 
 
 @app.route('/complete/<int:task_id>/')
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
-    db.session.commit()
-    flash('The task is complete. Nice.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id or session['role'] == "admin":
+        task.update({"status": "0"})
+        db.session.commit()
+        flash('The task is complete. Nice.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only update tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.session.commit()
-    flash('The task was deleted. Why not add a new one?')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id or session['role'] == "admin":
+        task.delete()
+        db.session.commit()
+        flash('The task was deleted. Why not add a new one?')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only delete tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
-
+        
