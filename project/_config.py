7201ec5984e94b8b5370bb09eebf204112d79c45
Dana Kock <danakock@me.com>
2015-06-27 12:49:27 -0700

    Add Error Logging

--- project/_config.py
+++ project/_config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
+DEBUG = False
 
 # define the full path for the DATABASE
 DATABASE_PATH = os.path.join(basedir, DATABASE)
