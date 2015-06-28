e7459117200c3aa2074b6e40d7d269aa29472423
Dana Kock <danakock@me.com>
2015-06-28 08:32:28 -0700

    Create API to GET Taks

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 # project/__init__.py
 
+
 import datetime
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -12,28 +13,35 @@ db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.tasks.views import tasks_blueprint
+from project.api.views import api_blueprint
+
 
 # register our blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+app.register_blueprint(api_blueprint)
+
+
+# error handlers
 
 @app.errorhandler(404)
 def page_not_found(error):
-	if app.debug is not True:
-		now = datetime.datetime.now()
-		r = request.url
-		with open('error.log', 'a') as f:
-			current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
-			f.write("\n404 error at {}: {}".format(current_timestamp, r))
-	return render_template('404.html'), 404
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n404 error at {}: {} ".format(current_timestamp, r))
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
-	db.session.rollback()
-	if app.debug is not True:
-		now = datetime.datetime.now()
-		r = request.orl
-		with open('error.log', 'a') as f:
-			current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
-			f.write("\n500 error at {}: {}".format(current_timestamp, r))
-	return render_template('500.html'), 500
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n500 error at {}: {} ".format(current_timestamp, r))
+    return render_template('500.html'), 500
